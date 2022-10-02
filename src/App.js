cf5af13e720f667036ef199677b496edac32f033
Ishikawa Tatsuki <papaymango@gmail.com>
2022-10-03 08:25:27 +0900

    On branch master Changes to be committed:     modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	modified:   src/App.js 	modified:   src/index.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Card,Button,ButtonToolbar } from 'reactstrap';
 
 function App() {
   return (
@@ -22,4 +24,19 @@ function App() {
   );
 }
 
-export default App;
+const FunctionalComponent = () => {
+  const [count, setCount] = React.useState(0);
+ 
+  return (
+    <div className='bg-dark text-center'>
+      <Card className='bg-danger' color='secondary' outline>Count: {count}</Card>
+      <ButtonToolbar className='bg-info'>
+        <Button className='me-2 px-2 py-1' outline color='primary' onClick={() => setCount(count + 1)}>Click</Button>
+        <Button className='px-2 py-1' outline color='primary' onClick={() => setCount(0)}>Reset</Button>
+      </ButtonToolbar>
+    </div>
+  );
+ };
+
+ export {FunctionalComponent};
+ export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { FunctionalComponent } from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,6 +14,12 @@ root.render(
   </React.StrictMode>
 );
 
+const comp = ReactDOM.createRoot(document.getElementById('comp'));
+comp.render(
+  <React.StrictMode>
+    <FunctionalComponent />
+  </React.StrictMode>
+);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
